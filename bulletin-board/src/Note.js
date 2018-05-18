01bb5bb4514fad64355ff78b9af027b6723f9c94
Guinsly Mondésir <guinslym@gmail.com>
2018-05-17 23:26:17 -0400

    finish 4-2 Adding state to the Note component

--- bulletin-board/src/Note.js
+++ bulletin-board/src/Note.js
@@ -1,33 +1,68 @@
 import React,{Component} from 'react';
 import FaPencil from 'react-icons/lib/fa/pencil';
 import FaTrash from 'react-icons/lib/fa/trash';
+import FaFloppyO from 'react-icons/lib/fa/floppy-o';
 
 export default class Note extends Component {
 
     constructor(props){
     	super(props);
-    	this.state = {};
+    	this.state = {
+        editing: false,
+      }
 
       this.edit = this.edit.bind(this)
       this.remove = this.remove.bind(this)
+      this.save = this.renderDisplay.bind(this)
+      this.renderForm = this.renderForm.bind(this)
+      this.renderDisplay = this.renderDisplay.bind(this)
     }
 
-    edit(){
-      alert('editing notes')
+    edit() {
+  		this.setState({
+  			editing: true
+  		})
+  	}
+
+  	remove() {
+  		alert('removing note')
+  	}
+
+
+    	save() {
+    		alert('alert')
+    	}
+
+    	renderForm() {
+    		return (
+    			<div className="note">
+    				<form>
+    					<textarea ref={input => this._newText = input}/>
+    					<button onClick={this.save}><FaFloppyO /></button>
+    				</form>
+    			</div>
+    		)
+    	}
+
+    renderDisplay() {
+  		return (
+  			<div className="note">
+  				<p>Learn React</p>
+  				<span>
+  					<button onClick={this.edit} id="edit"><FaPencil /></button>
+  					<button onClick={this.remove} id="remove"><FaTrash /></button>
+  				</span>
+  			</div>
+  		)
+  	}
+
+    render(){
+        if(this.state.editing){
+          return this.renderForm()
+        }else{
+          return this.renderDisplay()
+        }
     }
 
-    remove(){
-      alert('removing notes')
-    }
-    render() {
-        return (
-            <div className="note">
-                <p>Learning React</p>
-                <span>
-                  <button id="edit" onClick={this.edit}><FaPencil /></button>
-                  <button id="remove" onClick={this.remove}><FaTrash /></button>
-                </span>
-            </div>
-        );
-    }
+
 }
