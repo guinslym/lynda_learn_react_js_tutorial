45e8ea94294b5adc16aa2d8b8dc31b22112127c7
Guinsly Mondésir <guinslym@gmail.com>
2018-05-17 23:10:36 -0400

    finish 4-1

--- bulletin-board/src/Note.js
+++ bulletin-board/src/Note.js
@@ -1,14 +1,31 @@
 import React,{Component} from 'react';
+import FaPencil from 'react-icons/lib/fa/pencil';
+import FaTrash from 'react-icons/lib/fa/trash';
 
 export default class Note extends Component {
 
+    constructor(props){
+    	super(props);
+    	this.state = {};
+
+      this.edit = this.edit.bind(this)
+      this.remove = this.remove.bind(this)
+    }
+
+    edit(){
+      alert('editing notes')
+    }
+
+    remove(){
+      alert('removing notes')
+    }
     render() {
         return (
             <div className="note">
                 <p>Learning React</p>
                 <span>
-                  <button>Edit</button>
-                  <button>Remove</button>
+                  <button id="edit" onClick={this.edit}><FaPencil /></button>
+                  <button id="remove" onClick={this.remove}><FaTrash /></button>
                 </span>
             </div>
         );
