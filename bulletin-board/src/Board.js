0920279261b0817c1fd57275c9a768690a5bca7e
Guinslym <guinslym@users.noreply.github.com>
2018-05-19 14:48:41 -0400

    finish 5-1

--- bulletin-board/src/Board.js
+++ bulletin-board/src/Board.js
@@ -1,38 +1,55 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import Note from './Note'
 
-export default class Board extends Component {
+class Board extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			notes: [
+				{
+					id: 0,
+					note: "Call Lisa"
+				},
+				{
+					id: 1,
+					note: "Email John"
+				},
+				{
+					id: 2,
+					note: "Order printer ink"
+				}
+			]
+		}
+		this.eachNote = this.eachNote.bind(this)
+		this.update = this.update.bind(this)
+	}
 
-  constructor(props){
-  	super(props)
-  	this.state = {
-      notes: [
-        {
-          id: 33,
-          note:'Call Lisa'
-        },{
-          id: 34,
-          note:'Email John'
-        }
-      ],
-    }
+	update(newText, i) {
+		console.log('updating item at index', i, newText)
+		this.setState(prevState => ({
+			notes: prevState.notes.map(
+				note => (note.id !== i) ? note : {...note, note: newText}
+			)
+		}))
+	}
 
-    this.eachNote = this.eachNote.bind(this)
-  }
+	eachNote(note, i) {
+		return (
+			<Note key={i}
+				  index={i}
+				  onChange={this.update}>
+				  {note.note}
+		    </Note>
+		)
+	}
 
-  eachNote(note, i){
-    return (
-      <Note key={i} index={i} >
-        {note.note}
-      </Note>
-    )
-  }
-
-    render() {
-        return (
-            <div className="board">
-                {this.state.notes.map(this.eachNote)}
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="board">
+				{this.state.notes.map(this.eachNote)}
+			</div>
+		)
+	}
 }
+
+export default Board
